50d6b6e6e90985c0db89a2b57f0f004314d6be93
IdrisRoti <idrisomisakin@gmail.com>
2024-12-27 12:08:30 +0100

    fix: dynamically import react quill

--- components/forms/new-list-form/PropertyDescription.tsx
+++ components/forms/new-list-form/PropertyDescription.tsx
@@ -1,10 +1,18 @@
 "use client"
 
 import { CreateNewListContext } from '@/context/CreateNewListContext';
+import dynamic from 'next/dynamic';
 import React, { useContext, useState } from 'react'
 import { BsArrowRight } from 'react-icons/bs';
-import ReactQuill from 'react-quill-new';
 import 'react-quill-new/dist/quill.snow.css';
+
+const RichTextEditor = dynamic(
+    () => import("react-quill-new"),
+    { 
+        ssr: false ,
+        loading: () => <p>Loading ...</p>
+    }
+)
 
 const PropertyDescription = () => {
     const [value, setValue] = useState('');
@@ -17,7 +25,7 @@ const PropertyDescription = () => {
         <p className='text-sm text-textlight'>What do you have to say about your property?</p>
 
         <div className="relative mt-3 w-full rounded-lg">
-            <ReactQuill className='bg-transparent w-full' theme="snow" value={value} onChange={setValue} />
+            <RichTextEditor className='bg-transparent w-full' theme="snow" value={value} onChange={setValue} />
             {value && <button onClick={() => dispatch({ type: "ADD_PROPERTY_DESCRIPTION", payload: value})} className='absolute bottom-4 right-4 shadow-2xl size-10 rounded-full bg-brightPink hover:opacity-70 transition text-white grid place-items-center'><BsArrowRight className='size-5 font-bold' /></button>}
         </div>
     </div>
